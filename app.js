07c5df562385a16dda7bce39a87faf0b26b425f9
Galiant <antonijodev@gmail.com>
2019-01-18 22:24:29 +0000

    Displaying profile data

--- app.js
+++ app.js
@@ -23,3 +23,41 @@ const data = [{
     image: 'https://randomuser.me/api/portraits/female/30.jpg'
   }
 ];
+
+const profiles = profileIterator(data);
+
+// Next event
+document.getElementById('next').addEventListener('click', nextProfile);
+
+// Next profile display
+function nextProfile() {
+  const currentProfile = profiles.next().value;
+
+  document.getElementById('profileDisplay').innerHTML = `
+  <ul class="list-group">
+    <li class="list-group-item">Name: ${currentProfile.name}</li>
+    <li class="list-group-item">Age: ${currentProfile.age}</li>
+    <li class="list-group-item">Gender: ${currentProfile.gender}</li>
+    <li class="list-group-item">Preferences: Looking for ${currentProfile.lookingfor}</li>
+    <li class="list-group-item">Location: ${currentProfile.location}</li>
+  </ul>
+  `;
+
+  document.getElementById('imageDisplay').innerHTML = `<img src="${currentProfile.image}">`;
+}
+
+// Profile Iterator
+function profileIterator(profiles) {
+  let nextIndex = 0;
+
+  return {
+    next: function () {
+      return nextIndex < profiles.length ? {
+        value: profiles[nextIndex++],
+        done: false
+      } : {
+        done: true
+      }
+    }
+  };
+}
