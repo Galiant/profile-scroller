7ac899b6ecf2cefa3c6f71eff0c47b379ac2ac40
Galiant <antonijodev@gmail.com>
2019-01-18 22:07:55 +0000

    Created data for project

---
+++ app.js
@@ -0,0 +1,25 @@
+const data = [{
+    name: 'John Doe',
+    age: 32,
+    gender: 'male',
+    lookingfor: 'female',
+    location: 'Dublin IE',
+    image: 'https://randomuser.me/api/portraits/men/32.jpg'
+  },
+  {
+    name: 'Jenny Smith',
+    age: 28,
+    gender: 'female',
+    lookingfor: 'male',
+    location: 'Cork IE',
+    image: 'https://randomuser.me/api/portraits/female/28.jpg'
+  },
+  {
+    name: 'Monica White',
+    age: 30,
+    gender: 'female',
+    lookingfor: 'male',
+    location: 'Dublin IE',
+    image: 'https://randomuser.me/api/portraits/female/30.jpg'
+  }
+];
